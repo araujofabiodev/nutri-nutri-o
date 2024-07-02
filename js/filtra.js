30e708611fffb693e737c5b087101851b7658ef6
araujofabiodev <fabioaraujodevcoder@gmail.com>
2024-07-02 17:10:06 -0300

    Criando filtro de paciente

---
+++ js/filtra.js
@@ -0,0 +1,26 @@
+var campoFiltro = document.querySelector("#filtrar-tabela");
+
+campoFiltro.addEventListener("input", function() {
+    var pacientes = document.querySelectorAll(".paciente");
+    var valorFiltro = this.value.trim().toLowerCase();
+
+    if(this.value.length > 0){
+        for (var i = 0; i < pacientes.length; i++) {
+            var paciente = pacientes[i];
+            var tdNome = paciente.querySelector(".info-nome");
+            var nome = tdNome.textContent.trim().toLowerCase();
+    
+            if (valorFiltro === "" || nome.includes(valorFiltro)) {
+                paciente.classList.remove("invisivel");
+            } else {
+                paciente.classList.add("invisivel");
+            }
+        }
+    } else {
+        for (var i = 0; i < pacientes.length; i++) {
+            var paciente = pacientes[i];
+            paciente.classList.remove("invisivel");
+        }
+    }
+    
+});
